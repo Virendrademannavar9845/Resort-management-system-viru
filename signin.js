668f46fb9fac2aaa993d8778f94ce98d121ecaa5
Virendra Demannavar <demannavarvirendra@gmail.com>
2023-05-10 18:07:24 +0530

    Resort Management - Viru

---
+++ signin.js
@@ -0,0 +1,32 @@
+var login = document.getElementById( 'login' )
+
+login.addEventListener( 'click', () => {
+    event.preventDefault()
+    const name = document.getElementById( 'username' ).value
+    const password = document.getElementById( 'exampleInputPassword1' ).value
+    let userInfo = {}
+    userInfo.username = name
+    userInfo.password = password
+
+    let registeredUsers = localStorage.getItem( 'registeredUsers' )
+    registeredUsers = JSON.parse( registeredUsers )
+    //console.log( userInfo, registeredUsers )
+
+    //=========== Admin Login ==============
+
+    if ( name == 'admin' && password == 'admin' ) {
+        location.href = 'admin.html'
+        alert( 'admin logged in' )
+    }
+    else {
+        let index = 0;
+        while ( index < registeredUsers.length ) {
+            if ( name == registeredUsers[ index ].Username && password == registeredUsers[ index ].Password ) {
+                location.href = 'rooms.html'
+                break
+            }
+            alert( 'Incorrect username or password' )
+            index++
+        }
+    }
+} )
